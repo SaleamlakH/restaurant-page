16ef9a855bdbef3407e919eee0ac409683870d70
SaleamlakH <saleamlak.7h@gmail.com>
2026-02-07 22:12:28 +0300

    Feat: Add event listener to the home button

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import './style.css';
 import { appendHome } from './home.js';
 
+const home = document.querySelector('.home-btn');
+
 const main = document.querySelector('main');
 appendHome(main);
+
+home.addEventListener('click', () => {
+  appendHome(main);
+});
